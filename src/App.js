c00066ca9b4b40e114ff4549689b86ba38962dd3
Btissameq <qorchi.btissam@gmail.com>
2021-01-05 20:56:11 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React, {forwardRef  } from "react";
 import './App.css';
+import Tippy from '@tippy.js/react';
+import 'tippy.js/dist/tippy.css';
+
+const TooltipColor = () => {
+  return <p style={{color:"pink"}}>Tooltip Component</p>
+}
+
+const Child = forwardRef((props,ref) => {
+  return(
+    <div ref={ref}>
+      <div>First child</div>
+      <div>Second child</div>
+    </div>
+  )
+})
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={{marginBottom:'30px'}}>
+        <Tippy arrow={false} delay={1000} placement='left' content="Simple Tooltip">
+          <button>Hover me</button>
+        </Tippy>
+      </div>
+      <div style={{marginBottom:'30px'}}>
+        <Tippy content={<p style={{color:'red'}}>HTML Tooltip</p>}>
+          <button>Hover me</button>
+        </Tippy>
+      </div>
+      <div style={{marginBottom:'30px'}}>
+        <Tippy content={<TooltipColor></TooltipColor>}>
+          <button>Hover me</button>
+        </Tippy>
+      </div>
+      <div style={{marginBottom:'30px'}}>
+        <Tippy content={<TooltipColor></TooltipColor>}>
+          <Child></Child>
+        </Tippy>
+      </div>
     </div>
   );
 }
